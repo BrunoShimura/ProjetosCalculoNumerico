10cb6946fd8b10fff31555366a4fad8cbe9c028e
BrunoShimura <bruno_shimura@hotmail.com>
2020-09-07 20:16:00 -0300

    resolver

--- pages/exercicio3/main.js
+++ pages/exercicio3/main.js
@@ -1,33 +1,13 @@
 function resolver() {
-  var a = parseInt(document.form1.a.value);
-  var erro = parseInt(document.form1.erro.value)*-1;
-  var text = '<table class="table table-striped"> <thead> <tr> <th>Interações</th> <th>Resposta</th> ';
-  var res = 0;
-  console.log(Math.pow(10,erro));
-  for(var cont=1;;cont++){
-    res = a - (funcao(a)/derivada(a))
-    text += '<tr> <td>x'+cont+'</td> <td>'+
-    res
-    +'</td>';
-    if(Math.abs(res-a)>Math.pow(10,erro)){
-      a = res;
-      console.log(Math.abs(res-a))
-    }else{
-      document.getElementById("resposta" ).innerHTML ='Raiz procurada é '+res;
-      break;
-    }
-    
-  }
 
-  text += '</tbody> </table>';
-  document.getElementById("tabela" ).innerHTML = text;
+  var matriz = [[]];
+
+  for(var i=0;i<3;i++){
+    matriz[i] = [];
+    for(var j=0;j<4;j++){
+      matriz[i][j] = parseFloat(document.getElementById(""+i+j+"").value);
+    }
+  }
+  console.log(matriz)
+  console.log(document.getElementById("erro").value)
 }
-function funcao(a){
-  var b = Math.pow( 2.71828182846,a)+Math.pow(2, -a)+(2*Math.cos(a))-6;
-  return b;
-}
-function derivada(a){
-  var b = Math.pow(2.71828182846, a)-Math.log(a)*Math.pow(2, -a)-(2*Math.sin(a));
-  return b;
-}
-Math.abs()
