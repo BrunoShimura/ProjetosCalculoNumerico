a76c990b838089851579c9011167d54060a201b8
BrunoShimura <bruno_shimura@hotmail.com>
2020-09-09 21:40:52 -0300

    metodo-de-gauss-seidel

--- main.js
+++ main.js
@@ -4,3 +4,6 @@ function exercicio1(){
 function exercicio2(){
   window.location.href = "pages/exercicio2/index.html";
 }
+function exercicio3(){
+  window.location.href = "pages/exercicio3/index.html";
+}
