971e0d8861faa6f35185b47aa74fb02c7babcff7
BrunoShimura <bruno_shimura@hotmail.com>
2020-10-21 22:45:07 -0300

    atividade-integracaoo-numerica

--- pages/exercicio6/main.js
+++ pages/exercicio6/main.js
@@ -1,7 +1,9 @@
 function resolver(){
-  var x = []
-  x[0] = [1,1.5,2,2.5,3]
-  x[1] = [1,1.5,2,2.5,3]
+
+  var x = 1.8;
+  var y = 1.4;
+  console.log(x);
+  console.log(y);
   var h = 0.5
   for(i in x[0])
     x[1][i] = fun(x[0][i]) 
@@ -29,3 +31,18 @@ function fun(x){
   var e = 2.7182818284590452353602874
   return 1/(Math.pow(e,Math.pow(x,2)))
 }
+var x = 1.8;
+var y = 1.4;
+var h = (x-y)/4;
+var tabela = []
+tabela[0] = [0,0,0,0,0]
+tabela[0][0] = y
+var anterior = 0;
+for(var i = 1;i<5;i++) 
+  anterior = tabela[0][1-i]
+  tabela[0][i] = anterior + h
+
+for(i in tabela[0])
+  console.log(tabela[0][i]);
+console.log(tabela);
+console.log(h);
